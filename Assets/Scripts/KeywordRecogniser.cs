3f2268fce239cb893ca82cc9fd0f7df69d202698
scoob <scooby359@gmail.com>
2018-03-26 00:28:20 +0100

    Simple Microsoft voice input enabled. Recognises "start" and "describe". Just logs message to console for now.

---
+++ Assets/Scripts/KeywordRecogniser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class KeywordRecogniser : MonoBehaviour {
+
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start () {
+
+        //Create keywords for keyword recognizer
+        keywords.Add("start", () =>
+        {
+            // action to be performed when this keyword is spoken
+            Debug.Log("Voice Recognised: Start called");
+        });
+
+        keywords.Add("describe", () =>
+        {
+            // action to be performed when this keyword is spoken
+            Debug.Log("Voice Recognised: Describe called");
+        });
+
+        //Start recognizer
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        //Now register for the OnPhraseRecognized event
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+
+        //Start recognizer
+        keywordRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //Function to call when onphraserecognized
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+}
