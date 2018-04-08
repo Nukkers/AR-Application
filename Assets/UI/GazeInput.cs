6bddf7a4633f790a232bba629aecd2a7779650ef
Adam Maudsley <adam_maudsley@live.co.uk>
2018-04-08 17:47:15 +0100

    Fixed gaze input, improved it so it does't need to have a script on each canvas element and so that it supports multiple canvases.

--- Assets/UI/GazeInput.cs
+++ Assets/UI/GazeInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
+using System;
 
 /// <summary>
 /// Implements gaze-based navigation.
@@ -10,20 +10,34 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class GazeInput : MonoBehaviour
 {
-    GraphicRaycaster m_Raycaster;
-    PointerEventData m_PointerEventData;
-    EventSystem m_EventSystem;
-    Button lastSelected = null;
+    public GraphicRaycaster m_Raycaster = null;
+    public PointerEventData m_PointerEventData;
+    public EventSystem m_EventSystem;
+    public Camera mCamera;
+    public Transform mLastCanvas = null;
+    public Button lastSelected = null;
+    public const int uiLayerMask = 1 << 5; // TODO : Refactor me, generate a layer mask for UI objects.
     void Start()
     {
         //Fetch the Raycaster from the GameObject (the Canvas)
-        m_Raycaster = GetComponent<GraphicRaycaster>();
         //Fetch the Event System from the Scene
         m_EventSystem = GetComponent<EventSystem>();
+        mCamera = this.GetComponent<Camera>();
     }
 
     void Update()
     {
+        /* Update the looked at canvas */
+        /* Send a 'ray' from the middle of the screen towards our scene */
+        Ray ray = mCamera.ScreenPointToRay(new Vector3(mCamera.pixelWidth / 2, mCamera.pixelHeight / 2)); // cast from the center of the screen
+        RaycastHit rayHit;
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            if (rayHit.transform != mLastCanvas)
+                OnNewCanvasLookedAt(rayHit.transform);
+        }
+
+
         //Set up the new Pointer Event
         m_PointerEventData = new PointerEventData(m_EventSystem);
         //Set the Pointer Event Position to that of the mouse position
@@ -33,42 +47,48 @@ public class GazeInput : MonoBehaviour
         List<RaycastResult> results = new List<RaycastResult>();
 
         //Raycast using the Graphics Raycaster and mouse click position
-        m_Raycaster.Raycast(m_PointerEventData, results);
-
-        if (results.Count == 0)
+        if (m_Raycaster != null)
         {
-            if (lastSelected != null)
-            {
-                lastSelected.OnDeselect(null);
-                lastSelected = null;
-            }
-        }
+            m_Raycaster.Raycast(m_PointerEventData, results);
 
-        int count = 0;
-
-        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
-        foreach (RaycastResult result in results)
-        {
-            Debug.Log("Hit " + result.gameObject.name + " #" + count);
-            count++;
-            Button obj = result.gameObject.GetComponent<Button>(); // Surely there's a better way?
-            if (obj != null)
+            if (results.Count == 0)
             {
-                if (obj != lastSelected && lastSelected != null)
+                if (lastSelected != null)
+                {
                     lastSelected.OnDeselect(null);
+                    lastSelected = null;
+                }
+            }
 
-                obj.OnSelect(null);
-                lastSelected = obj;
-                break;
+            int count = 0;
+
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                Debug.Log("Hit " + result.gameObject.name + " #" + count);
+                count++;
+                Button obj = result.gameObject.GetComponent<Button>(); // Surely there's a better way?
+                if (obj != null)
+                {
+                    if (obj != lastSelected && lastSelected != null)
+                        lastSelected.OnDeselect(null);
+
+                    obj.OnSelect(null);
+                    lastSelected = obj;
+                    break;
+                }
             }
         }
-
         /* TODO : Make this less mobile specific */
-        if (Input.GetTouch(0).phase == TouchPhase.Began && lastSelected != null)
-            lastSelected.OnPointerClick(null);
-    
+        //if (Input.GetTouch(0).phase == TouchPhase.Began && lastSelected != null)
+        //    lastSelected.OnPointerClick(null);
+
     }
 
-
-
+    private void OnNewCanvasLookedAt(Transform newTransform)
+    {
+        mLastCanvas = newTransform;
+        m_Raycaster = newTransform.GetComponent<GraphicRaycaster>();
+    }
 }
+
